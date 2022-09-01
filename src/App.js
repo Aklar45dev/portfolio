554475c261ebeb330513b7a0acc993b2a38efce5
Aklar45dev <aklar45dev@gmail.com>
2022-09-01 12:29:33 -0400

    respo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import React, { useState, useEffect } from 'react'
+
 
 function App() {
+
+    const height = window.innerHeight+"px"
+    const width = window.innerWidth+"px"
+
   return (
     <div className="App">
-      <embed className='pdf' src="https://s3-bucket-cfp.s3.us-east-2.amazonaws.com/apps_CFPVD.pdf" />
+      <embed className='pdf' height={height} width={width} src="https://s3-bucket-cfp.s3.us-east-2.amazonaws.com/apps_CFPVD.pdf" />
     </div>
   );
 }
